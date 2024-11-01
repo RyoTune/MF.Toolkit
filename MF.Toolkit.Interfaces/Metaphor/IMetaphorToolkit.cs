48e8679c46f1b3c359c223c172f0b1d1be9a2c35
RyoTune <zutomayofan50@gmail.com>
2024-11-01 13:24:13 -0500

    Add Bits, Counters, File Redirection, and ability to add new Metaphor folders to API.

---
+++ MF.Toolkit.Interfaces/Metaphor/IMetaphorToolkit.cs
@@ -0,0 +1,47 @@
+﻿using MF.Toolkit.Interfaces.Metaphor.Models;
+
+namespace MF.Toolkit.Interfaces.Metaphor;
+
+/// <summary>
+/// General toolkit API.
+/// </summary>
+public interface IMetaphorToolkit
+{
+    /// <summary>
+    /// Add a Metaphor toolkit folder to load files from.
+    /// </summary>
+    /// <param name="modId">Mod ID.</param>
+    /// <param name="folderPath">Folder to add.</param>
+    void AddFolder(string modId, string folderPath);
+
+    /// <summary>
+    /// Set the value of a game BIT.
+    /// Internally, this works by replacing the actual value when the game retrieves it.
+    /// The actual data may or may not be edited depending on user setting, with no edit as default.
+    /// </summary>
+    /// <param name="bit">BIT to set.</param>
+    /// <param name="value">Value to set BIT to.</param>
+    void SetBit(int bit, bool value);
+
+    /// <summary>
+    /// Set the value of a game counter.
+    /// Internally, this works by replacing the actual value when the game retrieves it.
+    /// The actual data may or may not be edited depending on user setting, with no edit as default.
+    /// </summary>
+    /// <param name="counter">Counter to set.</param>
+    /// <param name="value">Value to set counter to.</param>
+    void SetCounter(int counter, int value);
+
+    /// <summary>
+    /// Redirect a file to a new path when it's loaded.
+    /// </summary>
+    /// <param name="ogPath">Original path to redirect.</param>
+    /// <param name="newPath">New path to redirect to.</param>
+    void RedirectFile(string ogPath, string newPath);
+
+    /// <summary>
+    /// Get list of user's owned DLC, once that data is available.
+    /// </summary>
+    /// <param name="ownedDlc">User's owned DLC.</param>
+    void CheckOwnedDlc(Action<GameDlc[]> ownedDlc);
+}
---
+++ MF.Toolkit.Interfaces/Metaphor/Models/GameDlc.cs
@@ -0,0 +1,14 @@
+﻿namespace MF.Toolkit.Interfaces.Metaphor.Models;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public enum GameDlc
+{
+    P1,
+    P2,
+    P3,
+    P4,
+    P5,
+    SMT4,
+    SMT5,
+    EO,
+}
